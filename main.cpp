926765edd71eba24dece10fbbbf9dac463f38460
Viktor Verebelyi <vviktor2@gmail.com>
2017-02-20 15:35:21 +0000

    Add menuItems context property so the GUI works

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QtQml/QQmlContext>
+#include <QtQml/QQmlEngine>
+#include <QJsonObject>
+#include <QVariantList>
 
 int main(int argc, char *argv[])
 {
@@ -8,6 +12,16 @@ int main(int argc, char *argv[])
     app.setOrganizationDomain("github.com/viktorgino");
     app.setApplicationName("headunit-gui");
     QQmlApplicationEngine engine;
+
+    int defaultMenuItem = 3;
+    QVariantList menuItems;
+    menuItems << QJsonObject {{"source","qrc:/qml/ClimateControl/CCLayout.qml"},{"image","icons/svg/thermometer.svg"},{"text","A/C"},{"color","#f44336"}}.toVariantMap()
+              << QJsonObject {{"source","qrc:/qml/Radio/RadioLayout.qml"},{"image","icons/svg/radio-waves.svg"},{"text","Radio"},{"color","#E91E63"}}.toVariantMap()
+              << QJsonObject {{"source","qrc:/qml/MediaPlayer/MediaPlayerLayout.qml"},{"image","icons/svg/music-note.svg"},{"text","Media player"},{"color","#3F51B5"}}.toVariantMap()
+              << QJsonObject {{"source",""},{"image","icons/gear-a.png"},{"text","Settings"},{"color","#2196F3"}}.toVariantMap();
+
+    engine.rootContext()->setContextProperty("menuItems", menuItems);
+    engine.rootContext()->setContextProperty("defaultMenuItem", defaultMenuItem);
     engine.load(QUrl(QStringLiteral("qrc:/qml/main.qml")));
 
     return app.exec();
